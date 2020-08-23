ac5501ae2fc4683f4221b1d81235d98c61b5c514
Angel <angel.prieto@osf.digital>
2020-08-23 20:06:13 -0300

    setting some styles

--- src/components/Character.js
+++ src/components/Character.js
@@ -6,53 +6,21 @@ export default class Character extends Component {
     character: PropTypes.object
   };
 
-  state = {
-    visible: Boolean = false,
-  }
-
-  handleClick(id) {
-    this.setState({visible: !this.state.visible})
-  }
-  
   render() {
     const { character } = this.props; 
-    if(!this.state.visible){
-      return (
-        <div>
-          <a href="#" onClick={() => this.handleClick(character.id)}>
-            <img
-              key={"i" + character.id}
-              className="Img"
-              width="100%"
-              src={character.image}
-              alt={character.name}
-            />
-            <div className="Description" key={character.id}>
-              <p className="p-desc">{character.name}</p>
-            </div>
-          </a>
+    return (
+      <div>
+        <img
+          key={"i" + character.id}
+          className="Img"
+          width="100%"
+          src={character.image}
+          alt={character.name}
+        />
+        <div className="Description" key={character.id}>
+          <p className="p-desc">{character.name}</p>
         </div>
-      );  
-    } else {
-      return (
-        <div class="character-details">
-          <a href="#" onClick={() => this.handleClick(character.id)}>
-            <img
-              key={"i" + character.id}
-              className="Img"
-              width="100%"
-              src={character.image}
-              alt={character.name}
-            />
-            <div className="Description" key={character.id}>
-              <p className="p-desc">{character.name}</p>
-              <p className="p-desc">{character.status}</p>
-              <p className="p-desc">{character.species}</p>
-              <p className="p-desc">{character.gender}</p>
-            </div>
-          </a>
-        </div>
-      );
-    }
+      </div>
+    );  
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,43 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Character from "../components/Character";
+import CharacterDetails from "../components/CharacterDetails";
 import "../styles.css";
 
 class Home extends Component {
-  static propTypes = {
-    data: PropTypes.object
-  };
+  constructor(props) { super(props); this.state = { visible: false, character: {} } }
+  
+  static propTypes = { data: PropTypes.object };
+  
+  handleClick(character) { 
+    this.setState({visible: !this.state.visible, character: character}) 
+  }
+
   render() {
     const { data } = this.props;
-    return (
-      <div className="Home">
-        <h1 className="home-title">Characters</h1>
-        <div className="home-list">
-          {data.characters.results.map((character) => {
-            return <Character key={"c" + character.id} character={character} />;
-          })}
+    if(!this.state.visible){ 
+      return (
+        <div className="Home">
+          <h1 className="home-title">Characters</h1>
+          <div className="home-list">
+            {data.characters.results.map((character) => {
+              return <a key={"a" + character.id}  href={"#"+character.name} onClick={() => this.handleClick(character)}> 
+                      <Character key={"c" + character.id} character={character} />
+                    </a>;
+            })}
+          </div>
         </div>
-      </div>
-    );
+      );
+    }else{
+      return(
+        <div className="CharacterDetails">
+          <div>
+            <img onClick={() => this.handleClick(this.state.character)} title="Go back" class="go-back" src="https://realtimevfx.com/uploads/default/original/2X/9/9ec795306ce4c382f785537845c3c798aba60d07.gif"></img>
+          </div>
+          <CharacterDetails key={"cd" + this.state.character.id} character={this.state.character} />
+        </div>
+      )
+    }
   }
 }
 
